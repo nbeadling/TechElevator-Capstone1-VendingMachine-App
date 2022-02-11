50a807f361d36a87c2d4f6f5c8296658cf9e9331
Nicholas Beadling <nbeadling@hotmail.com>
2022-02-11 11:02:40 -0500

    push exercise

--- capstone/Capstone/DisplayMenu.cs
+++ capstone/Capstone/DisplayMenu.cs
@@ -35,13 +35,14 @@ namespace Capstone
 
         public static void PurchaseMeun()
         {
+            Purchase purchase = new Purchase();
             Console.WriteLine();
             Console.WriteLine("***Purchase Menu****\n");
-            int currentAmount = 0;
+            
             bool restart = false;
             while (!restart)
             {
-                Console.WriteLine($"Current Balance: ${currentAmount}.00 \n");
+                Console.WriteLine($"Current Balance: ${purchase.InputAmount()}.00 \n");
                 Console.WriteLine("(1) Feed Money");
                 Console.WriteLine("(2) Select Product");
                 Console.WriteLine("(3) Finish Transaction \n");
@@ -57,8 +58,8 @@ namespace Capstone
                     {
                         Console.Write("feed money: $1, $2, $5, or $10: ");
                         int moneyInput = int.Parse(Console.ReadLine());
-                        currentAmount += moneyInput;
-                        Console.WriteLine();
+                        purchase.InputAmount(moneyInput);
+                    
                     }
                     else if (userInput == 2)
                     {
@@ -67,45 +68,18 @@ namespace Capstone
                         //Added the below code (Nick's Notes)
                         Console.WriteLine("Please Enter number selection of the Product you wish to purchase: ");
                         string selection = Console.ReadLine();
-                      try
-                      {
 
-
-                    if (selection == "A1" || selection == "A2" || selection == "A3" || selection == "A4")
-                    {
-                        //Call the Chip Method That we create? 
-                    }
-
-                    else if (selection == "B1" || selection == "B2" || selection == "B3" || selection == "B4")
-                    {
-                        //Call the Candy Method that we create? 
-                    }
-
-                    else if (selection == "C1" || selection == "C2" || selection == "C3" || selection == "C4")
-                    {
-                        //Call the Beveragle Method we create? 
-                    }
-
-                    else if (selection == "D1 " || selection == "D2" || selection == "D3" || selection == "D4")
-                    {
-                        //call the Gum method we create? 
+                        
+                        purchase.ItemPrice(selection);
+                        Console.WriteLine($"Cost of item: {purchase.ItemPrice(selection)}");
+            
                     }
                     else
                     {
-                        Console.WriteLine("Not Valid Selection");
-                    }
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine(e.Message);
-                }
 
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Transcation Complete");
-                        Console.WriteLine("Thank you for using the Vending Machine!");
+                        purchase.PriceComparison();
+                        //Console.WriteLine("Transcation Complete");
+                        //Console.WriteLine("Thank you for using the Vending Machine!");
                         break;
                     }
                 }
---
+++ capstone/Capstone/Purchase.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public class Purchase
+    {
+        public decimal Price { get; set; }
+
+        public decimal CurrentAmount { get; set;  }
+
+
+        Dictionary<string, decimal> vendingItems = new Dictionary<string, decimal>()
+        {
+            {"A1", 3.05M },
+            {"A2", 1.45M },
+            {"A3", 2.75M },
+            {"A4", 3.65M},
+            {"B1", 1.80M },
+            {"B2", 1.50M },
+            {"B3", 1.50M },
+            {"B4", 1.75M },
+            {"C1", 1.25M },
+            {"C2", 1.50M },
+            {"C3", 1.50M },
+            {"C4", 1.50M },
+            {"D1", 0.85M },
+            {"D2", 0.95M },
+            {"D3", 0.75M },
+            {"D4", 0.75M }
+        };
+
+        public decimal InputAmount(int moneyInput)
+        {
+            //int currentAmount = 0;
+            //Console.Write("feed money: $1, $2, $5, or $10: ");
+            //int moneyInput = int.Parse(Console.ReadLine());
+            this.CurrentAmount += moneyInput;
+            Console.WriteLine();
+            return this.CurrentAmount;
+        }
+        public decimal InputAmount()
+        {
+            //int currentAmount = 0;
+            //Console.Write("feed money: $1, $2, $5, or $10: ");
+            //int moneyInput = int.Parse(Console.ReadLine());
+            //this.CurrentAmount += moneyInput;
+            Console.WriteLine();
+            return this.CurrentAmount;
+        }
+        public  decimal  ItemPrice(string selection)
+        {
+            this.Price = vendingItems[selection];
+            return this.Price; 
+        }
+
+        public void  PriceComparison()
+        {
+            if(this.Price > this.CurrentAmount)
+            {
+                Console.WriteLine("Insufficent Funds, Please Add More Money!");
+                
+            }
+            else
+            {
+                Console.WriteLine("Transcation Complete");
+                Console.WriteLine("Thank you for using the Vending Machine!");
+            }
+        }
+
+
+    }
+}
